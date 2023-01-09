c6870b157064176315fca8191546661a1df923e9
Alvaro Lupercio <lco3rr@gmail.com>
2023-01-09 15:15:39 -0500

    restructure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,9 +7,31 @@ return require'packer'.startup(function()
     use 'https://github.com/mbbill/undotree.git'
     use 'tpope/vim-fugitive'
     use 'https://github.com/preservim/nerdcommenter.git'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+    -- treesitter
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- lsp
+    use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+	-- LSP support
+	{'neovim/nvim-lspconfig'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+
+	-- Autocompletion
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
+	{'saadparwaiz1/cmp_luasnip'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-nvim-lua'},
+
+	-- Snippets
+	{'L3MON4D3/LuaSnip'},
+	{'rafamadriz/friendly-snippets'},
+    }
+}
+
 end)
