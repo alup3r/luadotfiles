3a03e3490acf541298f3c0d57c34da5fb4602b4e
lupera1 <alup3r@gmail.com>
2023-06-07 19:26:37 -0400

    structure change

---
+++ lua/lsp/init.lua
@@ -0,0 +1,21 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim', 'use', 'opts'},
+      },
+    },
+  },
+}
+
+lsp.setup()
