4fab5a4a7d74eb8ab2a782a4f0c117d031023e72
Alvaro Lupercio <lco3rr@gmail.com>
2024-01-21 16:57:42 -0500

    dotfile rebuild

--- lua/core/plugin_config/rose-pine.lua
+++ lua/core/plugin_config/rose-pine.lua
@@ -1,3 +1,3 @@
--- Color settings
+--vim.o.termguicolors = true
 vim.cmd[[colorscheme rose-pine]]
 vim.cmd[[highlight Normal guibg=none]]
---
+++ lua/core/vimconfigs.lua
@@ -0,0 +1,24 @@
+-- General settings
+HOME = os.getenv("HOME")
+
+-- spacing
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+
+vim.o.relativenumber = true
+vim.o.nu = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = HOME .. "/.config/undodir"
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.scrolloff = 8
+vim.api.nvim_set_option_value("colorcolumn", "80", {})
+vim.cmd[[set nohlsearch]]
