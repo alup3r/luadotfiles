8086b0fbf8e95e5af289423101896f69e6059124
aluper <aluper@amazon.com>
2024-10-06 10:56:22 -0400

    adds keymap for undotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,17 +3,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
 -- window navigation
 vim.keymap.set("n", "<leader>h", "<C-w>h")
 vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.keymap.set("n", "<leader>l", "<C-w>l")
 
-
 -- new buffer vertical split
 vim.api.nvim_set_keymap('n', '<C-w>n', ':vnew<CR>', { noremap = true, silent = true })
-
 
 -- make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")
@@ -21,3 +18,6 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")
 vim.keymap.set("n", "<C-y>", ":set invnumber invrelativenumber<CR>")
 
 vim.keymap.set("n", "<leader>g", ":VimwikiToggleListItem<CR>")
+
+-- Undotree mappings
+vim.keymap.set("n", "<leader>n", ":UndotreeToggle<CR>")
