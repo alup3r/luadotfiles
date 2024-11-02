65fd1b1fb0f1c6344ddc268764b9fd3076e6aac0
Alvaro Lupercio <aluper@amazon.com>
2024-11-02 11:01:57 -0400

    set telescope default mappings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -68,34 +68,13 @@ return {
                 }
             })
 
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<leader>cv", vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
 
-            -- set vim motion for <SPACE> + c + h to show code documentation
-            -- about the code the cursor is currently over if available
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-
-            -- set vim motion for <SPACE> + c + d to go where the code/variable
-            -- under the cursor was defined
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-
-            -- set vim motion for <SPACE> + c + a for displaying code action
-            -- suggestions for code diagnostics in both normal and visual mode
-            vim.keymap.set("n", "<leader>cv", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-
-            -- set vim motion for <SPACE> + c + r to display references for the
-            -- code under the cursor
-            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
-
-            -- set vim motion for <SPACE> + c + i to display implementations to
-            -- the code under the cursor
-            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
-
-            -- set vim motion for <SPACE> + c + <SHIFT>R to smartly rename the
-            -- code under the cursor
-            vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-
-            -- set vim motion for <SPACE> + c + <SHIFT>D to go where the
-            -- code/object was declared in the project (class file)
-            vim.keymap.set("n", "<leader>ci", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
         end
     }
 }
