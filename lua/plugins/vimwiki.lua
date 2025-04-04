59603e770ebdc899ba4eb56619161317a965d63c
Alvaro Lupercio <aluper@proton.me>
2025-04-04 14:17:52 -0400

    custom format and directory for vimwiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,3 +1,12 @@
 return {
   "vimwiki/vimwiki",
+  config = function()
+    vim.g.vimwiki_list = {
+      {
+        path = "~/Documents/vimwikiii/",
+        syntax = "markdown",
+        ext = ".md",
+      }
+    }
+  end
 }
